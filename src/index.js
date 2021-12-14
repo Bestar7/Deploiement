53aec97918f7d496a563a05c92b532e4456aa6f4
Bestar7 <71338224+Bestar7@users.noreply.github.com>
2021-12-14 08:45:28 +0100

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./stylesheets/main.css";
+
+import Navbar from "./components/NavBar/NavBar";
+import {Router} from "./Components/Router/Router";
+
+
+
+
+Router();
+
+Navbar();
+
+
+
