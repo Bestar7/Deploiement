d07bcb8ec57fc3adf1caabea4540e63d54e62a84
Joachim IPL <joachim.bastin@student.vinci.be>
2021-12-14 10:13:29 +0100

    rm Popper.js

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,16 +1,3 @@
-// When using Bootstrap to style components, the CSS is imported in index.js
-// However, the JS has still to be loaded for each Bootstrap's component that needs it.
-// Here, because our JS component 'Navbar' has the same name as Navbar Bootstrap's component
-// we change the name of the imported Bootstrap's 'Navbar' component
-import { Navbar as BootstrapNavbar} from "bootstrap";
-
-/**
- * Render the Navbar which is styled by using Bootstrap
- * Each item in the Navbar is tightly coupled with the Router configuration :
- * - the URI associated to a page shall be given in the attribute "data-uri" of the Navbar
- * - the router will show the Page associated to this URI when the user click on a nav-link
- */
-
 const Navbar = () => {
   const navbarWrapper = document.querySelector("#navbarWrapper");
   let navbar = `
